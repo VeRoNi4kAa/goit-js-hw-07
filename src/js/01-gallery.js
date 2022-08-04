be96667519c857b925af1573083082f22dbd4b12
Veronika Kovalenko <podaritebegemotika@gmail.com>
2022-08-04 19:56:39 +0300

    п

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const createMarkup = images => {
+    return images
+        .map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+    />
+    </a>
+</div>
+    `;
+        })
+        .join('');
+};
+const markup = createMarkup(galleryItems);
+const addMarkup = markup => {
+    galleryContainer.insertAdjacentHTML('beforeend', markup);
+};
+addMarkup(markup);
+
+let instance = basicLightbox;
+
+const onPress = e => {
+    if (e.key === 'Escape' && instance.visible()) {
+        instance.close();
+    }
+};
+const onClick = e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    } else {
+        instance = basicLightbox.create(`<img src="${e.target.dataset.source}">`);
+        instance.show();
+    }
+};
+
+galleryContainer.addEventListener('click', onClick);
+document.addEventListener('keydown', onPress);
+
+
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryCont = document.querySelector('.gallery');
+const galleryItemsString = galleryItems.map(item => `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+).join('');
+
+galleryCont.insertAdjacentHTML('afterbegin', galleryItemsString);
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'button',
+});
